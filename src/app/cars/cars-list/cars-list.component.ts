1429b4bb87d9f36654b5b63b1279643a54c7d1e3
michalfirszt <michalfiszu@gmail.com>
2018-05-01 14:22:36 +0200

    model car

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Car} from '../models/car';
 
 @Component({
   selector: 'app-cars-list',
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-list.component.less']
 })
 export class CarsListComponent implements OnInit {
+
+  cars : Car[] = [
+    {
+      id: 1,
+      model: 'm1',
+      plate: 'p1',
+      deliveryDate: 'deliveryDate1',
+      deadline: 'deadline1',
+      client: {
+        firstName: 'firstName1',
+        surname: 'surname1',
+      },
+      cost: 1,
+      isFullyDamaged: true
+    },
+    {
+      id: 2,
+      model: 'm2',
+      plate: 'p2',
+      deliveryDate: 'deliveryDate2',
+      deadline: 'deadline2',
+      client: {
+        firstName: 'firstName2',
+        surname: 'surname2',
+      },
+      cost: 2,
+      isFullyDamaged: false
+    }
+  ];
 
   constructor() { }
 
